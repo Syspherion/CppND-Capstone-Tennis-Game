7bece01a8a59618bf6986980d2ab4123f5128145
Syspherion <manuel.jaehnig@googlemail.com>
2023-02-04 11:18:33 +0100

    Config tries to initialize from properties file

--- src/config.cpp
+++ src/config.cpp
@@ -1,22 +1,93 @@
 #include "config.h"
+#include <sstream>
+#include <fstream>
+#include <iostream>
 
+std::map<std::string, std::string> Config::properties = []
+{
+    std::cout << "Start init properties\n";
+    std::map<std::string, std::string> properties;
+    std::ifstream infile("config.properties");
+    if (infile.is_open())
+    {
+        std::string line;
+
+        while (std::getline(infile, line))
+        {
+            std::istringstream iss(line);
+            std::string name, value;
+            if (!(iss >> name >> value))
+            {
+                std::cout << "Error while reading values from file"
+                          << "\n";
+                break;
+            } // error
+            std::cout << "Read values from file: " << name << " / " << value << "\n";
+            properties.insert(std::pair<std::string, std::string>(name, value));
+        }
+    }
+    else
+    {
+        std::cout << "Properties file not found!\n";
+    }
+
+    return properties;
+}();
+
+std::string Config::getValueFromProperties(std::string name)
+{
+    MAP::const_iterator pos = properties.find(name);
+    if (pos == properties.end())
+    {
+        return "";
+    }
+    else
+    {
+        return pos->second;
+    }
+}
 
 int Config::getRacketSize()
 {
-    return 5;
+    std::string size = getValueFromProperties("RacketSize");
+    if (size == "")
+    {
+        return 5;
+    }
+    else
+    {
+        return std::stoi(size);
+    }
 }
 
 std::string Config::getNamePlayerOne()
 {
-    return "Manuel";
+    std::string namePlayerOne = getValueFromProperties("NamePlayerOne");
+    if (namePlayerOne == "")
+    {
+        return "Manuel";
+    }
+    else
+    {
+        return namePlayerOne;
+    }
 }
 
 std::string Config::getNamePlayerTwo()
 {
-    return "Melanie";
+        std::string namePlayerOne = getValueFromProperties("NamePlayerTwo");
+    if (namePlayerOne == "")
+    {
+        return "Melanie";
+    }
+    else
+    {
+        return namePlayerOne;
+    }
 }
 
-SDL_Scancode Config::getRacketLeftKeyUp(){
+SDL_Scancode Config::getRacketLeftKeyUp()
+{
     return SDL_SCANCODE_W;
 }
 
--- src/config.h
+++ src/config.h
@@ -1,11 +1,14 @@
 #include <string>
 #include "SDL.h"
+#include <map>
 
 #ifndef CONFIG_H
 #define CONFIG_H
 
 class Config
 {
+typedef std::map<std::string, std::string>MAP;
+
 public:
    static int getRacketSize();
    static std::string getNamePlayerOne();
@@ -14,6 +17,16 @@ public:
    static SDL_Scancode getRacketLeftKeyDown();
    static SDL_Scancode getRacketRightKeyUp();
    static SDL_Scancode getRacketRightKeyDown();
+
+   static std::string test;
+
+
+
+private:
+static MAP properties;
+static std::string getValueFromProperties(std::string name);
+
 };
+
 
 #endif
